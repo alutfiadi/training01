e34a53159b7a574249fc641f7e2248e1eefb7745
Ahmad Lutfiadi <ahmad.lutfiadi@abyor.com>
2025-08-04 08:20:54 +0000

    feat: Edit Carrier Detail

--- webapp/controller/CarrierDetail.controller.js
+++ webapp/controller/CarrierDetail.controller.js
@@ -1,8 +1,8 @@
 sap.ui.define([
     "training01/controller/BaseController",
-    'sap/ui/core/Fragment'
+    "sap/m/MessageBox"
 ], (Controller,
-    Fragment
+    MessageBox
 ) => {
     "use strict";
 
@@ -26,18 +26,111 @@ sap.ui.define([
         _onRouteMatched: function (event) {
             let paramters = event.getParameters();
             let carrierIdparam = paramters.arguments.carrier;
-            let pathCarrier = this.modelFilght.createKey("/CarrierSet",{
+            this.pathCarrier = this.modelFilght.createKey("/CarrierSet", {
                 CarrierId: carrierIdparam
             })
 
             this.viewDetail.bindElement({
-                path: pathCarrier,
+                path: this.pathCarrier,
                 model: 'flight'
             });
 
+            //Bind Table Connection with expanded Connections from CarriesSet
+            this.tableConnection.setModel(this.modelFilght);
+            this.tableConnection.bindElement({
+                path: this.pathCarrier,
+                parameters: {
+                    expand: "Connections"
+                }
+            });
+
+            //Bind Table Connection with expanded Flights from CarriesSet
+            this.tableFlight.setModel(this.modelFilght);
+            this.tableFlight.bindElement({
+                path: this.pathCarrier,
+                parameters: {
+                    expand: "Flights"
+                }
+            });
+
+        },
+        onSavePress: function (oEvent) {
+            this._toggleButtonsAndView(false);
+
+            let carrierId = this.byId("carrierId").getValue(),
+                name = this.byId("nameInput").getValue(),
+                currencyCode = this.byId("currencyInput").getValue();
+
+            let payload = {
+                CarrierId: carrierId,
+                Name: name,
+                CurrencyCode: currencyCode
+            }
+
+            this.modelFilght.update(this.pathCarrier, payload, {
+                success: function (oData, oResponse) {
+                    // Success
+                    MessageBox.success("Success Update Carrier Information.");
+                }.bind(this),
+                error: function (oError) {
+                    let response = JSON.parse(oError.responseText);
+                    let errmessage = response.error.message.value;
+                    let message = "Failed to update carrier due to:  " + errmessage;
+                    MessageBox.error(message);
+                }
+            })
+
         },
 
-       
+        onEditPress: function (event) {
+            this._toggleButtonsAndView(true);
+        },
+
+        onCancelPress: function (event) {
+            this._toggleButtonsAndView(false);
+        },
+
+        _toggleButtonsAndView: function (isEdit) {
+            // Show the appropriate action buttons
+            this.byId("editButton").setVisible(!isEdit);
+            this.byId("saveButton").setVisible(isEdit);
+            this.byId("cancelButton").setVisible(isEdit);
+
+            this.byId("nameInput").setEditable(isEdit);
+            this.byId("currencyInput").setEditable(isEdit);
+        },
+
+        onSearchEdit: function (event) {
+            // Get the source control that fired the event
+            const sourceControl = event.getSource();
+
+            // Get the ID of the source control
+            const sourceID = sourceControl.getId();
+            const querySearch = event.getParameter("query");
+
+            if (sourceID === this.byId("sfConnection").getId()) {
+                var idTable = "connectionTable";
+                var searchFields = [
+                    { field: "ConnectionId", type: "EQ" },
+                    { field: "AirportFromId", type: "Contains" },
+                    { field: "AirportToId", type: "Contains" }
+                ];
+            } else if (sourceID === this.byId("sfFlight").getId()) {
+                var idTable = "flightTable";
+                var searchFields = [
+                    { field: "ConnectionId", type: "EQ" },
+                    { field: "PlaneTypeId", type: "Contains" }
+                ];
+            }
+
+            // console.log({aSearchFields})
+            if (!event.getParameter("clearButtonPressed")) {
+                this.onSearch(querySearch, searchFields, idTable);
+            } else {
+                this.onSearch("", [], idTable);
+            }
+
+        }
 
     });
 });
